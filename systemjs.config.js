92129afd83554c32d51639e2a6525824d85f6591
Muthusamy Jayaraman <jaymuthus@gmail.com>
2019-03-04 12:19:24 -0800

    Moved the references for angular, zone, systemjs to local

--- systemjs.config.js
+++ systemjs.config.js
@@ -14,7 +14,10 @@
     },
     paths: {
       // paths serve as alias
-      'npm:': 'https://unpkg.com/'
+      'npm:': 'https://unpkg.com/',
+      'local:': 'node_modules',
+      'pl:': 'https://unpkg.com/'
+
     },
     // map tells the System loader where to look for things
     map: {
@@ -22,11 +25,12 @@
       'app': 'src',
 
       // angular bundles
+      'angular': 'local:angular/angular.js',
       '@angular/core': 'npm:@angular/core@5/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common@5/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler@5/bundles/compiler.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser@5/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/platform-browser-dynamic': 'pl:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/router/upgrade': 'npm:@angular/router/bundles/router-upgrade.umd.js',
@@ -36,9 +40,9 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs@5.0.1',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'ts':                        'npm:plugin-typescript@5.2.7/lib/plugin.js',
-      'typescript':                'npm:typescript@2.2.1/lib/typescript.js',
+      'angular-in-memory-web-api': 'pl:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'ts':                        'local:plugin-typescript/lib/plugin.js',
+      'typescript':                'local:typescript/lib/typescript.js',
 
     },
     // packages tells the System loader how to load when no filename and/or no extension
@@ -55,14 +59,14 @@
         defaultExtension: 'js'
       }
     },
-    /*meta: {
+    meta: {
       'angular': {
         format: 'global',
       },
-      'angular-route': {
+      /*'angular-route': {
         format: 'global',
-      },
-    } */
+      },*/
+    } 
   });
 
 })(this);
